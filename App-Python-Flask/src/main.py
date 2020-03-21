b07d918ef5301d0edc683d7774803f47a77fa569
monia beji <monia.elbeji@gmail.com>
2020-03-21 19:14:31 +0100

    nounou

--- App-Python-Flask/src/main.py
+++ App-Python-Flask/src/main.py
@@ -3,8 +3,8 @@ from flask import Flask, jsonify, request
 
 from flask_cors import CORS
 
-#from .entities.entity import Session, engine, Base
-#from .entities.exam import Exam, ExamSchema
+from entities.entity import Session, engine, Base
+from entities.exam import Exam, ExamSchema
 # ... other import statements ...
 
 # creating the Flask application
@@ -19,7 +19,7 @@ print(app)
 @app.route('/')
 def hello_world():
     return 'Hey, we have Flask in a Docker container!'
-'''
+
 @app.route('/exams')
 def get_exams():
     # fetching from the database
@@ -53,6 +53,6 @@ def add_exam():
     new_exam = ExamSchema().dump(exam)
     session.close()
     return jsonify(new_exam), 201
-'''
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8081)
